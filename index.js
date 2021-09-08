a8c5c1762519dce58bca4f0045d38d51fdf5c6bc
Aakriti <github.com/aakriti-sethi>
2021-09-08 18:05:22 +0530

    Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.toggle')
+const showacse = document.querySelector('.showcase')
+
+        menuToggle.addEventListener('click', function(){
+            menuToggle.classList.toggle('active')
+            showacse.classList.toggle('active')
+        })
